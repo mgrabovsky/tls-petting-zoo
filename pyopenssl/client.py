4c56852c43f955e912212c68a90e711e9355dba1
Matěj Grabovský <matej@mgrabovsky.net>
2019-10-24 19:24:24 +0200

    pyopenssl: Require strong ciphersuites

--- pyopenssl/client.py
+++ pyopenssl/client.py
@@ -36,14 +36,17 @@ if __name__ == '__main__':
 
     # Only allow TLS 1.2 or better.
     context = ssl.Context(ssl.TLSv1_2_METHOD)
+    context.set_options(ssl.OP_NO_SSLv2 | ssl.OP_NO_SSLv3 | ssl.OP_NO_TLSv1 |
+            ssl.OP_NO_TLSv1_1)
     context.set_default_verify_paths()
-    # Verify the server certificate during handshake. This should check for
-    # possible expiration and validity of chain (trusted root and valid
-    # intermediates).
-    # NOTE: Does not check for hostname or IP match by default in current
-    # version.
-    context.set_verify(ssl.VERIFY_PEER | ssl.VERIFY_FAIL_IF_NO_PEER_CERT,
-            verify_certificate)
+    # Verify the server certificate during handshake. This should check for possible
+    # expiration and validity of chain (trusted root and valid intermediates).
+    # NOTE: Does not check for hostname or IP match by default in current version.
+    # TODO: Does this check for the presence of a certificate as well? Would a server
+    # that sent no certificate pass?
+    context.set_verify(ssl.VERIFY_PEER, verify_certificate)
+    # Only allow the most secure ciphersuites available in TLS >= 1.2.
+    context.set_cipher_list(b'AES256:+HIGH:!aNULL:!kDHE:!kRSA')
 
     with socket.create_connection((hostname, port)) as sock:
         logger.info('Socket connection established.')
